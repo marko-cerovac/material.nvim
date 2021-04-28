c33eb9c531860e9ad65cfc42b675cd3380351e2d
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-04-28 17:40:08 +0530

    removed unwanted print statements

--- lua/material/functions.lua
+++ lua/material/functions.lua
@@ -6,6 +6,7 @@ end
 -- Change_style takes a style name as a parameter and switches to that style
  local change_style = function (style)
      vim.g.material_style = style
+     print(style)
      package.loaded['material'] = nil
      package.loaded['material.util'] = nil
      package.loaded['material.theme'] = nil
@@ -17,9 +18,7 @@ end
 -- Toggle_style takes no parameters toggles the style on every function call
  local toggle_style = function (print_info)
     local switch = { "darker", "lighter", "palenight", "oceanic", "deep ocean" }
-    print(vim.g.material_style_switch)
     vim.g.material_style_switch = (vim.g.material_style_switch % table.getn(switch)) + 1
-    print(vim.g.material_style_switch)
   change_style(switch[vim.g.material_style_switch])
   if (print_info == true) then
       print(vim.g.material_style)
