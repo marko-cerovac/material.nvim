687fb2a7c6ca51a3562b6f609cbc8badd90544fa
Robert Timm <mail@rtti.de>
2021-05-19 22:27:34 +0200

    sync lualine and stock statusline colors
    This sets the stock statusline background colors to the same colors as
    the most dominant background colors in lualine. For lualine, the active
    skin contrast is slightly reduced (from title/white/black to fg) in
    order to make the statusline stand out a little less, the disabled skin
    fg color is changed from disabled to text in order to provide good
    readability while maintaining way less contrast in a disabled lualine
    compared to an active one.

--- lua/lualine/themes/material-nvim.lua
+++ lua/lualine/themes/material-nvim.lua
@@ -4,34 +4,34 @@ local material = {}
 
 material.normal = {
 	a = {fg = colors.bg, bg = colors.accent, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.fg, bg = colors.active},
 	c = {fg = colors.fg, bg = colors.selection},
 }
 
 material.insert = {
 	a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.fg, bg = colors.active},
 }
 
 material.visual = {
 	a = {fg = colors.bg, bg = colors.purple, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.fg, bg = colors.active},
 }
 
 material.replace = {
 	a = {fg = colors.bg, bg = colors.red, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.fg, bg = colors.active},
 }
 
 material.command = {
 	a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.fg, bg = colors.active},
 }
 
 material.inactive = {
-  a = {fg = colors.disabled, bg = colors.bg, gui = 'bold'},
-  b = {fg = colors.disabled, bg = colors.bg},
-  c = {fg = colors.disabled, bg = colors.selection}
+  a = {fg = colors.text, bg = colors.bg, gui = 'bold'},
+  b = {fg = colors.text, bg = colors.bg},
+  c = {fg = colors.text, bg = colors.selection}
 }
 
 return material
