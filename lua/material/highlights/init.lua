e4ff40aa2a8ad60742baca6aa852abbd304c5ee5
marko-cerovac <marko.cerovac16@gmail.com>
2022-10-20 13:16:40 +0200

    fixed the find_style() function

--- lua/material/highlights/init.lua
+++ lua/material/highlights/init.lua
@@ -147,8 +147,10 @@ M.main_highlights.editor = function()
         LineNr           = { fg = e.line_numbers },
         CursorLineNr     = { fg = e.accent },
         DiffAdd          = { fg = g.added, reverse = true },
+        -- DiffAdded        = { fg = g.added, reverse = true },
         DiffChange       = { fg = g.modified },
         DiffDelete       = { fg = g.removed, reverse = true },
+        -- DiffRemoved      = { fg = g.removed, reverse = true },
         DiffText         = { fg = g.modified, reverse = true },
         ModeMsg          = { fg = e.accent }, -- 'showmode' message (e.g., "-- INSERT -- ")
         NonText          = { fg = e.disabled },
