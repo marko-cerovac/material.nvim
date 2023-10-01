176f5995f07a241e4e0da29a8d4208cdcfb81bd2
marko-cerovac <marko.cerovac16@gmail.com>
2023-10-01 22:38:59 +0200

    added new indent blankline highlights

--- lua/material/highlights/plugins/indent-blankline.lua
+++ lua/material/highlights/plugins/indent-blankline.lua
@@ -6,8 +6,13 @@ local M = {}
 
 M.load = function()
     local plugin_hls = {
+        -- old indent blankline hl-s
         IndentBlanklineChar        = { fg = e.border },
         IndentBlanklineContextChar = { fg = e.disabled },
+
+        -- new indent blankline hl-s
+        IblIndent = { fg = e.border },
+        IblScope  = { fg = e.disabled },
     }
 
     return plugin_hls
